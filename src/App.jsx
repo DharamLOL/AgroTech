618346a475358af94a9152a0331ba6406004c7a7
DharamLOL <dharam.moraes@gmail.com>
2025-09-18 21:02:46 -0300

    Fix App.jsx / Merge Pauline

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Vendas from "./pages/Vendas";
 import Home from "./pages/Home";
 import Doar from "./pages/Doar";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
         <Route path="/Vendas" element={<Vendas />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Doar" element={<Doar />} />
