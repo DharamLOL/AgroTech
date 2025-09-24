5cb398c32e71e981b993003168eb8434813165ab
FSS <83908013+FSSDevX@users.noreply.github.com>
2025-09-24 18:34:17 -0300

    Fale Conosco V1.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Vendas from "./pages/Vendas";
 import PerfilCliente from "./pages/PerfilCliente";
 import PerfilEmpresa from "./pages/PerfilEmpresa";
+import FaleConosco from "./pages/FaleConosco";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Vendas />} />
         <Route path="/perfilcliente" element={<PerfilCliente />}/>
         <Route path="/perfilempresa" element={<PerfilEmpresa/>}/>
+        <Route path="/faleconosco" element={<FaleConosco/>}/>
       </Routes>
     </BrowserRouter>
   );
